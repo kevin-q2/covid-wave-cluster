df8067645f8b684e007234651aeaa24fa764bd3f
Kevin Quinn <kquinn@mbook.local>
2025-05-13 10:36:37 -0400

    state data cleaning

--- data/state/cleaning/gov_response.py
+++ data/state/cleaning/gov_response.py
@@ -1,28 +1,27 @@
 import pandas as pd
 import numpy as np
 
-# Read entire dataset, taken straight from the source:
-# https://health.google.com/covid-19/open-data/raw-data
-epi = pd.read_csv('../data/oxford-government-response.csv', index_col = 0)
-
-# Filter to US States and Territories,
-# for which location_key is a 2-letter state code
-pattern = r'^US_[A-Z]{2}$'
-filtered_epi = epi.loc[epi['location_key'].str.match(pattern, na = False)]
-
-# Find the unique dates (row indices) and locations (column indices)
-dates = np.sort(filtered_epi.index.value_counts().index)
-columns = np.sort(filtered_epi.location_key.value_counts().index)
-date_index = {dates[i]:i for i in range(len(dates))}
+# Read from the Oxford covid-policy-dataset
+gov = pd.read_csv(
+    '../covid-policy-dataset/data/OxCGRT_compact_subnational_v1.csv'
+)
+us = gov.loc[gov.CountryCode == 'USA']
+locations = us.RegionCode.value_counts().index
+# Using Alaska as an example
+ak = us.loc[us.RegionCode == 'US_AK']
+dates = pd.to_datetime(ak.Date, format = "%Y%m%d")
+date_index = {dates.iloc[i]:i for i in range(len(dates))}
 
 # Fill a data array with daily, new confirmed cases
-data = np.zeros((len(dates), len(columns)))
+data = np.zeros((len(dates), len(locations)))
 data[:] = np.nan
-for l, col in enumerate(columns):
-    confirmed = filtered_epi.loc[filtered_epi.location_key == col].new_confirmed
-    idx = [date_index[i] for i in confirmed.index]
-    data[idx, l] = confirmed
+for l, loc in enumerate(locations):
+    loc_data = us.loc[us.RegionCode == loc]
+    containment_health = loc_data.ContainmentHealthIndex_Average
+    loc_dates = pd.to_datetime(loc_data.Date, format = "%Y%m%d")
+    idx = [date_index[i] for i in loc_dates]
+    data[idx, l] = containment_health
 
 # Save as a dataframe        
-data = pd.DataFrame(data, index = dates, columns = columns)
+data = pd.DataFrame(data, index = dates, columns = locations)
 data.to_csv('data/state/gov_response.csv')
